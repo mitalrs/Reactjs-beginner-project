58f76e85aa1717d183dcec79acab97cafe3bfd1f
mitalrs <mitalsapkale1999@gmail.com>
2023-03-16 18:53:50 +0530

    work on header fun

--- MarkDownEdit/src/App.jsx
+++ MarkDownEdit/src/App.jsx
@@ -4,10 +4,9 @@ import "./App.css";
 function App() {
   const textarearef = useRef();
   function insertAtCursor(start, end) {
-    console.log(textarearef);
     let cursorStart = textarearef.current.selectionStart;
     let cursorEnd = textarearef.current.selectionEnd;
-    console.log(cursorStart, cursorEnd);
+
 
     textarearef.current.value =
       textarearef.current.value.substring(0, cursorStart) +
@@ -19,9 +18,32 @@ function App() {
         textarearef.current.value.length
       );
   }
+
   function handleBold() {
     insertAtCursor("**", "**");
   }
+  function handleItelic() {
+    insertAtCursor("*", "*");
+  }
+  function handleEraser() {
+    insertAtCursor("~~", "~~");
+  }
+  function handleCode() {
+    insertAtCursor("`", "`");
+  }
+  function handleHeaderOne(){
+    insertAtCursor("\n\n# ", "")
+  }
+  function handleHeaderTwo(){
+    insertAtCursor("\n\n## ", "")
+  }
+  function handleHeaderThree(){
+    insertAtCursor("\n\n### ", "")
+  }
+  function handleHeaderFour(){
+    insertAtCursor("\n\n#### ", "")
+  }
+
   return (
     <div className="App">
       <h1>Markdown Editor</h1>
@@ -39,14 +61,14 @@ function App() {
           header
         </button>
         <button onClick={handleEraser}>Eraser</button>
-        <button onClick={handleBulletUl}>bulletUL</button>
+        {/* <button onClick={handleBulletUl}>bulletUL</button>
         <button onClick={handleBulletOl}>bulletOL</button>
         <button onClick={handleCheckBox}>checkBOx</button>
-        <button onClick={handleBlokquote}>Blockquote</button>
+        <button onClick={handleBlokquote}>Blockquote</button> */}
         <button onClick={handleCode}>code</button>
-        <button onClick={handleTable}>table</button>
+        {/* <button onClick={handleTable}>table</button>
         <button onClick={handleLink}>Link</button>
-        <button onClick={handleimg}>img</button>
+        <button onClick={handleimg}>img</button> */}
       </nav>
       <textarea className="textArea" ref={textarearef}></textarea>
     </div>
@@ -55,6 +77,4 @@ function App() {
 
 export default App;
 
-// https://stackoverflow.com/questions/33245017/react-modifying-textarea-values
-// https://stackoverflow.com/questions/66770617/how-to-wrap-specific-text-inside-a-div-in-a-span-in-react
-// how to work bold button in react
+
